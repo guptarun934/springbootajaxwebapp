d4851d2a8a11c35670f8f27034be885832e9ecec
Arun Gupta <shivam.arun2012@gmail.com>
2018-09-17 19:29:55 +0530

    initial Spring Boot Ajax Project

---
+++ src/main/java/com/ar/SpringbootajaxwebappApplication.java
@@ -0,0 +1,12 @@
+package com.ar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootajaxwebappApplication {
+
+    public static void main (String[] args) {
+        SpringApplication.run(SpringbootajaxwebappApplication.class, args);
+    }
+}
